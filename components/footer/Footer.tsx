218b60a5b41b16b4f797758372f717ed3518fde3
turembekov <turembekov@vk.com>
2023-01-16 01:47:45 +0600

    added footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import Socials from "../socials/Socials";
+
+const Footer: FC = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-wrap flex-wrap">
+          <div className="footer-block">
+            <h4>Узнайте больше о нашей компаний</h4>
+            <p>присоединяйтесь к нам в соц сетях</p>
+            <Socials />
+          </div>
+          <div className="mission">
+            Наша цель <span>доверие</span> наших клиентов
+          </div>
+          <div className="copyright">
+            &copy; КұмТансСауда {new Date().getFullYear()}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import Header from "../header/Header";
 import gilroy from "../../fonts";
+import Footer from "../footer/Footer";
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,6 +11,7 @@ const Layout: FC<LayoutProps> = ({ children }) => (
   <div className={gilroy.className}>
     <Header />
     {children}
+    <Footer />
   </div>
 );
 
